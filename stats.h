6c06cf0635eaaae1448005b95a39e07d713fd95e
Hamza Ali <Hamza2.ali@live.uwe.ac.uk>
2026-07-10 18:34:30 +0100

    stats.h just defines the functions that calculate the mean, max, min and standard deviation. They will later show what they do in stats.c

---
+++ stats.h
@@ -0,0 +1,16 @@
+//
+// Created by hamz0 on 02/07/2026.
+//
+
+#ifndef STATS_H
+#define STATS_H
+
+double calc_mean(const double *values, int count);
+double calc_min(const double *values, int count);
+double calc_max(const double *values, int count);
+double calc_std_dev(const double *values, int count, double mean);
+
+
+
+
+#endif // STATS_H
